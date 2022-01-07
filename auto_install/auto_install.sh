21582368baeef5faa33c1f04df4853721bc3a152
RedAnonymusITA2021 <78427215+RedAnonymusITA@users.noreply.github.com>
2022-01-07 21:50:47 +0100

    password generatore e stato inserito

--- auto_install/auto_install.sh
+++ auto_install/auto_install.sh
@@ -1,9 +1,11 @@
 echo "choose ip_tools or ip_tools_GUI"
 echo "1=ip_tools_GUI"
 echo "2=ip_tools for cmd and teminal linux,Mac"
+echo "3=Generatore_Password_GUI
 read i
 condizione=1
 condizione2=2
+condizione3=3
 if  [ $i = $condizione ];then 
     git clone https://github.com/RedAnonymusITA/ip_tools_GUI.git
 
@@ -21,5 +23,12 @@ elif [ $i=$condizione2 ];then
     sudo chmod a+x start.sh
 
     ./start.sh
+if [ $i=$condizione3];then
+   git clone  https://github.com/RedAnonymusITA/auto_install-GUI.git
+   cd auto_install_GUI
+   cd auto_install 
+   chmod +x auto_install.sh
+   ./auto_install.sh
+   
      
 fi
